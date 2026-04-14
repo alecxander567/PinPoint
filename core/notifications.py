3a3ba597b1f044d0cec0312ac048dbca978aa4ae
alecxander567 <alecxanderespaldon21@gmail.com>
2026-04-14 12:26:25 +0800

    Commit

--- core/notifications.py
+++ core/notifications.py
@@ -6,28 +6,27 @@ import json
 import base64
 from django.conf import settings
 
-if not firebase_admin._apps:
-    firebase_cred_b64 = os.getenv("FIREBASE_SERVICE_ACCOUNT")
-    if firebase_cred_b64:
-        firebase_cred_json = json.loads(base64.b64decode(firebase_cred_b64))
-        cred = credentials.Certificate(firebase_cred_json)
-    else:
-        cred = credentials.Certificate(
-            os.path.join(settings.BASE_DIR, "serviceAccountKey.json")
-        )
-    firebase_admin.initialize_app(cred)
+
+def _init_firebase():
+    if not firebase_admin._apps:
+        firebase_cred_b64 = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+        if firebase_cred_b64:
+            firebase_cred_json = json.loads(base64.b64decode(firebase_cred_b64))
+            cred = credentials.Certificate(firebase_cred_json)
+        else:
+            cred = credentials.Certificate(
+                os.path.join(settings.BASE_DIR, "serviceAccountKey.json")
+            )
+        firebase_admin.initialize_app(cred)
 
 
 def send_push_notification(token, title, body):
+    _init_firebase()
     message = messaging.Message(
-        notification=messaging.Notification(
-            title=title,
-            body=body,
-        ),
+        notification=messaging.Notification(title=title, body=body),
         token=token,
     )
-    response = messaging.send(message)
-    return response
+    return messaging.send(message)
 
 
 def notify_owner(item):
@@ -37,7 +36,6 @@ def notify_owner(item):
         owner = User.objects.get(id=item.owner_id)
     except User.DoesNotExist:
         return
-
     if owner.fcm_token:
         try:
             send_push_notification(
